af893ba782f242d2af93ef2e6af5677749b57d91
Federico Minutoli <40361744+DiTo97@users.noreply.github.com>
2023-05-16 10:06:14 +0200

    Added the DRD metric

--- metrics.py
+++ metrics.py
@@ -19,9 +19,125 @@ from common import Bitmap
 from morphology import bwmorph_thin
 
 
-def drd(references: Bitmap, preds: Bitmap) -> None:
+def drd(
+    references: Bitmap, 
+    preds: Bitmap, 
+    eps: float = 1e-6, 
+    block_size: int = 8, 
+    block_mask_size: int = 5
+) -> nptyping.NDArray[float]:
     """The distance reciprocal distortion (DRD) metric"""
-    pass
+    batch_size, height, width = references.shape
+
+    u_references = np.zeros((batch_size, height + 2, width + 2))
+    u_references[:, 1:height + 1, 1:width + 1] = references
+
+    II = np.cumsum(u_references, 1)  # integral images
+    II = np.cumsum(II, 2)
+
+    block_size_square = block_size ** 2
+    nonuniform_pixels = np.zeros(batch_size)  # NUBN
+
+    for idx in range(1, height - block_size + 1, block_size):
+        for jdx in range(1, width - block_size + 1, block_size):
+            block_sum = (
+                  II[:, idx + block_size - 1, jdx + block_size - 1]
+                - II[:, idx - 1, jdx + block_size - 1]
+                - II[:, idx + block_size - 1, jdx - 1]
+                + II[:, idx - 1, jdx - 1]
+            )
+
+            allwhite = block_sum == 0
+            allblack = block_sum == block_size_square
+
+            nonuniform = ~(allwhite | allblack)
+            nonuniform_pixels += nonuniform
+
+    weights = np.zeros((batch_size, block_mask_size, block_mask_size))
+
+    half_mask_size = (block_mask_size + 1) / 2
+    half_mask_size = int(half_mask_size)
+
+    for idx in range(block_mask_size):
+        for jdx in range(block_mask_size):
+            nume = np.sqrt(
+                  (idx + 1 - half_mask_size) ** 2 
+                + (jdx + 1 - half_mask_size) ** 2
+            )
+
+            if nume == 0:
+                continue
+
+            weights[:, idx, jdx] = 1 / nume
+
+    norm_weights = weights / np.sum(weights, axis=(1, 2), keepdims=True)
+
+    u_references_resized = np.zeros((
+        batch_size, 
+        height + half_mask_size + 1, 
+         width + half_mask_size + 1
+    ))
+
+    u_references_resized[
+        :, 
+        half_mask_size - 1:height + half_mask_size - 1, 
+        half_mask_size - 1: width + half_mask_size - 1
+    ] = references
+
+    u_preds_resized = np.zeros((
+        batch_size, 
+        height + half_mask_size + 1, 
+         width + half_mask_size + 1
+    ))
+
+    u_preds_resized[
+        :,
+        half_mask_size - 1:height + half_mask_size - 1, 
+        half_mask_size - 1: width + half_mask_size - 1
+    ] = preds
+
+    neg_u_references_resized = 1 - u_references_resized
+    neg_u_preds_resized = 1 - u_preds_resized
+
+    fpositives_resized = neg_u_preds_resized * u_references_resized
+    fpositives_resized = fpositives_resized.astype(bool)
+
+    fnegatives_resized = u_preds_resized * neg_u_references_resized
+    fnegatives_resized = fnegatives_resized.astype(bool)
+
+    difference = fpositives_resized | fnegatives_resized
+
+    _, height_resized, width_resized = difference.shape
+
+    sum_score = np.zeros(batch_size)
+
+    for idx in range(half_mask_size - 1, height_resized - half_mask_size + 1):
+        for jdx in range(half_mask_size - 1, width_resized - half_mask_size + 1):
+            equal = difference[:, idx, jdx] == 0
+
+            local_u_references = u_references_resized[
+                :,
+                idx - half_mask_size + 1:idx + half_mask_size, 
+                jdx - half_mask_size + 1:jdx + half_mask_size
+            ]
+
+            local_u_preds = u_preds_resized[:, idx, jdx]
+
+            local_fpositives = (1 - local_u_references) * local_u_preds 
+            local_fnegatives = (1 - local_u_preds) * local_u_references
+            
+            local_difference = local_fpositives | local_fnegatives
+
+            local_score = np.sum(local_difference * norm_weights, axis=(1, 2))
+            local_score = local_score * ~equal
+
+            sum_score += local_score
+
+    nume = sum_score
+    deno = nonuniform_pixels + eps
+
+    score = nume / deno
+    return score
     
 
 def fmeasure(references: Bitmap, preds: Bitmap, eps: float = 1e-6) -> nptyping.NDArray[float]:
@@ -97,6 +213,8 @@ def psnr(references: Bitmap, preds: Bitmap) -> nptyping.NDArray[float]:
 class DIBCO:
     num_thin_iters: int = -1
     eps: float = 1e-6
+    block_size: int = 8, 
+    block_mask_size: int = 5
         
     def __call__(self, references: Bitmap, preds: Bitmap) -> typing.Dict[str, float]:
         if references.ndim not in [2, 3]: 
@@ -121,7 +239,7 @@ class DIBCO:
         if references.shape[0] != preds.shape[0]:
             raise ValueError("The references and preds bitmaps do not have the same number of examples")
         
-        batch_drds = None
+        batch_drds = drd(references, preds, self.eps, self.block_size, self.block_mask_size)
         batch_fmeasures = fmeasure(references, preds, self.eps)
         batch_pseudo_fmeasures = psuedo_fmeasure(references, preds, self.eps, num_iters=self.num_thin_iters)
         batch_psnrs = psnr(references, preds)
